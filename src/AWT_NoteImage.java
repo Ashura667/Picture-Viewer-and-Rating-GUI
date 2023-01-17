e3d91bf911a551022cc5c71acf4c79abc8f0544d
Ashura <116311276+Ashura667@users.noreply.github.com>
2023-01-17 23:41:12 +0100

    Update AWT_NoteImage.java

--- src/AWT_NoteImage.java
+++ src/AWT_NoteImage.java
@@ -1,6 +1,13 @@
+import javax.swing.JRadioButton;
 
 public class AWT_NoteImage {
 	AWT_NoteImage() {
+		JRadioButton rBtn1 = new JRadioButton("1");
+		JRadioButton rBtn2 = new JRadioButton("2");
+		JRadioButton rBtn3 = new JRadioButton("3");
+		JRadioButton rBtn4 = new JRadioButton("4");
+		JRadioButton rBtn4 = new JRadioButton("5");
+		
 		
 	}
 
